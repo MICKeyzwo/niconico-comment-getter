f33b7e11f2d491f76b2601aa0442b806a69fff95
MICKeyzwo <singing.programmer@gmail.com>
2018-06-23 21:50:04 +0900

    リクエストをクソみたいなxmlで送る莫迦を止めてjsonリクエストの方式に改めた＆so~の動画コメントに対応した

--- test/test2.js
+++ test/test2.js
@@ -2,7 +2,7 @@
 
 const Ncg = require("../lib/niconico-comment-getter.js");
 
-const myNcg = new Ncg(process.argv[3], process.argv[4]);
+const myNcg = new Ncg(process.argv[2], process.argv[3]);
 
-myNcg.getComment(process.argv[2])
+myNcg.getComment("1484628320")
 .then(res => console.log(res));
